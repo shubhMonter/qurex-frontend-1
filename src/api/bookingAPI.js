a3480c532bd6d8396516ce3d3e48ff825ffed9e4
shubham <shubham.dixit@edelweiss.in>
2022-12-07 02:08:33 +0530

    maooing done

--- src/api/bookingAPI.js
+++ src/api/bookingAPI.js
@@ -1,22 +1,39 @@
-import {get, headers } from ".";
+import { get, headers, put } from ".";
 import { BaseSetting } from "../utils/common";
 
-
 const BookingAPI = {
-    getBookingById: async(id, token) => {
-        try {
-            const response = await get(
-                BaseSetting.ApiDomain + `/booking/${id}`, {...headers, ['x-auth-token']: token }
-            );
-            if (response.data.status === 1) {
-                return response.data.data
-            } else {
-                return false
-            }
-        } catch (error) {
-            return false
-        }
+  getBookingById: async (id, token) => {
+    try {
+      const response = await get(BaseSetting.ApiDomain + `/booking/${id}`, {
+        ...headers,
+        ["x-auth-token"]: token,
+      });
+      if (response.data.status === 1) {
+        return response.data.data;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
     }
-}
+  },
 
-export default BookingAPI
+  cancelBookingById: async (id, token) => {
+    try {
+      const response = await put(
+        BaseSetting.ApiDomain + `/booking/cancel/${id}`,
+        {},
+        { ...headers, ["x-auth-token"]: token }
+      );
+      if (response.data.status === 1) {
+        return response.data.data;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
+  },
+};
+
+export default BookingAPI;
