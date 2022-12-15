5c47d60b5953a31551d41ba20db20e00080c778e
Yogesh Nogia <yogeshnogia@gmail.com>
2022-12-15 17:08:03 +0530

    Video Token Added

--- src/common/components/AgoraRTC/setting.ts
+++ src/common/components/AgoraRTC/setting.ts
@@ -6,7 +6,7 @@ import {
 
 export const appId = "487313108aca464bb93de894daedc887";
 export const token =
-  "007eJxTYDhzRTrr5GVRyVdTVRpLF1Z9n14eGfAq8nPBkpNVwR5C14sVGEwszI0NjQ0NLBKTE03MTJKSLI1TUi0sTVISU1OSLSzMr0+amdwQyMiwRH4PMyMDBIL4rAyBpUWpFQwMAN7ZIYs=";
+  "007eJxTYHggm5DqeCBY/a/M/MN7EjijJfo+7TS7cIND4vYh+fAzZ7gUGEwszI0NjQ0NLBKTE03MTJKSLI1TUi0sTVISU1OSLSzMf7PNTm4IZGSwr05jYmSAQBCflSGwtCi1goEBADvgHyw=";
 
 export const config: ClientConfig = { mode: "rtc", codec: "vp8" };
 export const useClient = createClient(config);
